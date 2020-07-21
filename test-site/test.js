ae19d57166239aa02c7205c34ab57e8ff89bd9d2
Galya <gufkz.tkrbyf@yandex.ru>
2020-07-21 13:19:28 +0300

    Добавлена заготовка для модального окна 'Добавить/Редактировать'

--- test-site/test.js
+++ test-site/test.js
@@ -1,6 +1,13 @@
 const tbody = document.querySelector('#table-container');
 const buttons = document.querySelector('#buttons');
-const modal = document.querySelector('#modal-container');
+const modal = document.querySelector('#modal-wrapper');
+const modalCont = document.querySelector('#modal-container');
+const saveBnt = document.querySelector('#save');
+const closeBtn = document.querySelector('#close');
+
+const closeFunc = () => {
+    modal.classList.remove('open');
+};
 
 buttons.addEventListener('click', ({target}) => {
     switch (target.id) {
@@ -9,12 +16,28 @@ buttons.addEventListener('click', ({target}) => {
             break;
         };
         case 'update': {
+            // вывод модального окна для редакторования данных выбранного пользователя
             console.log('update');
             break;
         };
         case 'remove': {
+            // обработка запроса на удаление выбранного сотрудника из БД
             console.log('remove');
             break;
         };
     }
+});
+
+modalCont.addEventListener('change', () => {
+    closeBtn.removeEventListener('click', closeFunc);
+    closeBtn.addEventListener('click', () => {
+        alert('Вы не сохранили данные');
+    })
 })
+
+saveBnt.addEventListener('click', () => {
+    // обработка запроса на сохранение введенных данных
+    closeFunc();
+});
+
+closeBtn.addEventListener('click', closeFunc);

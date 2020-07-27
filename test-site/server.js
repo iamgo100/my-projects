a60f258b0c9f435cbbd3fea435aa232ebe3f090b
Галя Елкина <gufkz.tkrbyf@yandex.ru>
2020-07-27 23:30:50 +0300

    Добавлены бд, папка с фотографиями, отрисовка данных бд. Исправлены кнопки

--- test-site/server.js
+++ test-site/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 // const jsonParser = express.json();
 
 app.use(express.static(__dirname + '/site'));
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html')
+});
+
+app.post('/db', (req, res) => {
+    const fileContent = fs.readFileSync(__dirname + '/site/db.txt', 'utf8');
+    res.send(fileContent);
 });
 
 app.listen(8080, () => {
--- test-site/site/test.js
+++ test-site/site/test.js
@@ -1,13 +1,45 @@
 const tbody = document.querySelector('#c-table');
 const buttons = document.querySelector('#buttons');
+
 const comModal = document.querySelector('#w-modal');
 const comModalCont = document.querySelector('#c-modal');
 const msaveBnt = document.querySelector('#ms');
 const mcloseBtn = document.querySelector('#mc');
+
 const warningModal = document.querySelector('#w-warning');
 const warningModalCont = document.querySelector('#c-warning');
-const wsavwBtn = document.querySelector('#ws');
+const wsaveBtn = document.querySelector('#ws');
 const wcloseBtn = document.querySelector('#wc');
+
+const close = new CustomEvent('close');
+const save = new CustomEvent('save');
+const warning = new CustomEvent('warning');
+
+const getData = async () => {
+    const res = await fetch('http://localhost:8080/db', {method: "POST"});
+    const data = await res.json();
+    return data;
+};
+
+const renderTable = async () => {
+    const staff = await getData();
+    staff.forEach(el => {
+        const preview = `<img src="${el.preview}" alt="${el.name} ${el.lastName}" />`;
+        const remote = `<input type="checkbox" checked="${el.remote}" disabled />`;
+        const addr = el.address;
+        const addressString = `г. ${addr.city}, ${addr.street} ${addr.house}, кв. ${addr.flat}`;
+        tbody.innerHTML += `<tr>
+            <td>${preview}</td>
+            <td>${el.name}</td>
+            <td>${el.lastName}</td>
+            <td>${el.dateOfBirth}</td>
+            <td>${el.age}</td>
+            <td>${el.post}</td>
+            <td>${remote}</td>
+            <td>${addressString}</td>
+        </tr>`;
+    });
+};
 
 buttons.addEventListener('click', ({target}) => {
     switch (target.id) {
@@ -28,24 +60,43 @@ buttons.addEventListener('click', ({target}) => {
     }
 });
 
-const closeFunc = (modal) => modal.classList.remove('open');
-const warning = () => warningModal.classList.add('open');
-const saveFunc = () => {
-    console.log('save'); // обработка запроса на сохранение введенных данных
-    mcloseBtn.removeEventListener('click', warning);
-    mcloseBtn.addEventListener('click', closeFunc(comModal));
+// функция очищения формы от введенных данных
+// добавить ее в функцию закрывания модального окна
+
+const closeFunc = () => comModal.classList.remove('open');
+const warningFunc = () => warningModal.classList.add('open');
+const removeWarning = () => {
+    mcloseBtn.removeEventListener('click', warningFunc);
+    mcloseBtn.addEventListener('click', closeFunc);
 };
 
-msaveBnt.addEventListener('click', saveFunc);
-mcloseBtn.addEventListener('click', closeFunc(comModal));
+ const saveFunc = () => {
+    // сохраняет данные
+};
 
 comModalCont.addEventListener('change', () => {
-    mcloseBtn.removeEventListener('click', closeFunc(comModal));
-    mcloseBtn.addEventListener('click', warning);
+    mcloseBtn.removeEventListener('click', closeFunc);
+    mcloseBtn.addEventListener('click', warningFunc);
+})
+
+mcloseBtn.addEventListener('click', closeFunc);
+
+msaveBnt.addEventListener('click', () => {
+    // saveFunc();
+    removeWarning();
 });
 
-wsavwBtn.addEventListener('click', () => {
-    saveFunc();
-    closeFunc(warningModal);
+wcloseBtn.addEventListener('click', () => {
+    warningModal.classList.remove('open');
+    closeFunc();
+    removeWarning();
 });
-wcloseBtn.addEventListener('click', closeFunc(warningModal));
+
+wsaveBtn.addEventListener('click', () => {
+    // saveFunc();
+    warningModal.classList.remove('open');
+    closeFunc();
+    removeWarning();
+});
+
+renderTable();
